e5513bfc4d80bc56f07e7c44646e231c9fec70d9
Lucas Gallegos <lucasgallegos@utexas.edu>
2019-04-11 19:01:40 -0500

    Fixed data for sia5 dynamic response. Double check stewart.py for corret usage with correct robot

--- pandas_servo_angles.py
+++ pandas_servo_angles.py
@@ -13,8 +13,11 @@ rtd = 180/math.pi
 
 data1 = pd.read_csv("hebi_drop3.csv") #reads the data from the csv file
 data2 = pd.read_csv("ur3_angles_drop4_kalman.csv")
+data3 = pd.read_csv("angles_drop2_sia5_corrected.csv")
 
 time = data1["%time"]-(data1["%time"][0]) #Pulls and normalizes the data to start from t=0
+time = (time.values)/1e9 #converts to np array
+
 
 angle1_hebi = data1["field.Angle1"] #See pandas documentation on how to pull data 
 angle2_hebi = data1["field.Angle2"]
@@ -23,10 +26,6 @@ angle3_hebi = data1["field.Angle3"]
 angle4_hebi = data1["field.Angle4"]
 angle5_hebi = data1["field.Angle5"]
 angle6_hebi = data1["field.Angle6"]
-
-time = time.values #converts to np array
-
-print(time)
 
 angle1_hebi = angle1_hebi.values
 angle2_hebi = angle2_hebi.values
@@ -39,6 +38,7 @@ angle6_hebi = angle6_hebi.values
 #ur3
 
 time_ur3 = data2["%time"]-(data2["%time"][0])
+time_ur3 = (time_ur3.values)/1e9 #converts to np array
 
 angle1_ur3 = data2["field.Angle1"] #See pandas documentation on how to pull data 
 angle2_ur3 = data2["field.Angle2"]
@@ -48,8 +48,6 @@ angle4_ur3 = data2["field.Angle4"]
 angle5_ur3 = data2["field.Angle5"]
 angle6_ur3 = data2["field.Angle6"]
 
-time_ur3 = time_ur3.values #converts to np array
-
 angle1_ur3 = angle1_ur3.values
 angle2_ur3 = angle2_ur3.values
 angle3_ur3 = angle3_ur3.values
@@ -58,62 +56,90 @@ angle4_ur3 = angle4_ur3.values
 angle5_ur3 = angle5_ur3.values
 angle6_ur3 = angle6_ur3.values
 
+#sia5
+
+time_sia5 = data3["%time"]-(data3["%time"][0]) #Pulls and normalizes the data to start from t=0
+time_sia5 = (time_sia5.values)/1e9 #converts to np array
+
+
+angle1_sia5 = data3["field.Angle1"] #See pandas documentation on how to pull data 
+angle2_sia5 = data3["field.Angle2"]
+angle3_sia5 = data3["field.Angle3"]
+
+angle4_sia5 = data3["field.Angle4"]
+angle5_sia5 = data3["field.Angle5"]
+angle6_sia5 = data3["field.Angle6"]
+
+angle1_sia5 = angle1_sia5.values
+angle2_sia5 = angle2_sia5.values
+angle3_sia5 = angle3_sia5.values
+
+angle4_sia5 = angle4_sia5.values
+angle5_sia5 = angle5_sia5.values
+angle6_sia5 = angle6_sia5.values
+
 
 
 
 #Forces
 plt.subplot(6,2,1)
 plt.plot(time,-angle1_hebi*rtd-19, label='Series Elastic Robot')
-plt.plot(time_ur3-1.5e9,-angle1_ur3*rtd-10, label='Rigid UR3 Robot')
+plt.plot(time_ur3-1.5,-angle1_ur3*rtd-10, label='Rigid UR3 Robot')
+plt.plot(time_sia5-1.7,-angle1_sia5*rtd+40, label='Rigid SIA5 Robot')
 plt.legend()
 plt.xlabel('time [s]')
 plt.ylabel('Angle 1 [deg]')
-plt.xlim(0,0.8e10)
-plt.title('Haptic Feedback Device - Dynamic Response',fontsize=16)
+plt.xlim(0,5)
+#plt.title('Haptic Feedback Device - Dynamic Response',fontsize=16)
 
 plt.subplot(6,2,3)
 plt.plot(time,angle2_hebi*rtd+25, label='Series Elastic Robot')
-plt.plot(time_ur3-1.5e9,angle2_ur3*rtd, label='Rigid UR3 Robot')
+plt.plot(time_ur3-1.5,angle2_ur3*rtd, label='Rigid UR3 Robot')
+plt.plot(time_sia5-1.7,angle2_sia5*rtd+40, label='Rigid SIA5 Robot')
 plt.legend()
 plt.xlabel('time [s]')
 plt.ylabel('Angle 2 [deg]')
-plt.xlim(0,0.8e10)
+plt.xlim(0,5)
 
 
 plt.subplot(6,2,5)
 plt.plot(time,-angle3_hebi*rtd-25, label='Series Elastic Robot')
-plt.plot(time_ur3-1.5e9,-angle3_ur3*rtd, label='Rigid UR3 Robot')
+plt.plot(time_ur3-1.5,-angle3_ur3*rtd, label='Rigid UR3 Robot')
+plt.plot(time_sia5-1.7,-angle3_sia5*rtd-28, label='Rigid SIA5 Robot')
 plt.legend()
 plt.xlabel('time [s]')
 plt.ylabel('Angle 3 [deg]')
-plt.xlim(0,0.8e10)
+plt.xlim(0,5)
 
 
 plt.subplot(6,2,7)
 plt.plot(time,angle4_hebi*rtd-10, label='Series Elastic Robot')
-plt.plot(time_ur3-1.5e9,angle4_ur3*rtd, label='Rigid UR3 Robot')
+plt.plot(time_ur3-1.5,angle4_ur3*rtd+2, label='Rigid UR3 Robot')
+plt.plot(time_sia5-1.7,angle4_sia5*rtd+5, label='Rigid SIA5 Robot')
 plt.legend()
 plt.xlabel('time [s]')
 plt.ylabel('Angle 4 [deg]')
-plt.xlim(0,0.8e10)
+plt.xlim(0,5)
 #plt.title('Torque')
 
 plt.subplot(6,2,9)
-plt.plot(time,-angle5_hebi*rtd+5, label='Series Elastic Robot')
-plt.plot(time_ur3-1.5e9,-angle5_ur3*rtd-5, label='Rigid UR3 Robot')
+plt.plot(time,-angle5_hebi*rtd+10, label='Series Elastic Robot')
+plt.plot(time_ur3-1.5,-angle5_ur3*rtd-8, label='Rigid UR3 Robot')
+plt.plot(time_sia5-1.7,-angle5_sia5*rtd+15, label='Rigid SIA5 Robot')
 plt.legend()
 #plt.xlabel('time [s]')
 plt.ylabel('Angle 5 [deg]')
-plt.xlim(0,0.8e10)
+plt.xlim(0,5)
 
 
 plt.subplot(6,2,11)
 plt.plot(time,angle6_hebi*rtd+17, label='Series Elastic Robot')
-plt.plot(time_ur3-1.5e9,angle6_ur3*rtd, label='Rigid UR3 Robot')
+plt.plot(time_ur3-1.5,angle6_ur3*rtd+3, label='Rigid UR3 Robot')
+plt.plot(time_sia5-1.7,angle5_sia5*rtd-17, label='Rigid SIA5 Robot')
 plt.legend()
 plt.xlabel('time [s]')
 plt.ylabel('Angle 6 [deg]')
-plt.xlim(0,0.8e10)
+plt.xlim(0,5)
 
 plt.show()
 
